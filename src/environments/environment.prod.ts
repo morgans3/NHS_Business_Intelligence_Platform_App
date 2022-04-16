d98295c2ac56a795a8dd140e84f446c1edc45202
Stewart Morgan <stewartmorgan86@gmail.com>
2022-04-16 12:56:11 +0100

    added in production file variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  name: 'Production',
+  version: '1.0.0',
+  userNameDev: 'Demo.User@example.com',
+  homepage: 'dashboard',
+  appName: 'NHS BI Platform',
 };
