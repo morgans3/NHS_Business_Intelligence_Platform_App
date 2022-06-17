ecd1c420ac5e093042cc834e0e02b944b188ae26
alexseymourdev <alexseymourdev@gmail.com>
2022-06-17 14:42:13 +0100

    adding patient identifyable data as guard for admin section

--- src/app/applications/suicide_prevention/default/default.routing.ts
+++ src/app/applications/suicide_prevention/default/default.routing.ts
@@ -3,6 +3,7 @@ import { LandingComponent } from "./Landing/Landing.component";
 import { AdminComponent } from "./Admin/Admin.component";
 import { IncidentsComponent } from "./Incidents/Incidents.component";
 import { IncidentFormComponent } from "./Incidents/IncidentForm/IncidentForm.component";
+import { PidGuard } from "src/app/_guards/pid.guard";
 
 export const DefaultRoutes: Routes = [
     {
@@ -12,6 +13,7 @@ export const DefaultRoutes: Routes = [
     {
         path: "admin",
         component: AdminComponent,
+        canActivate: [PidGuard],
     },
     {
         path: "incidents",
